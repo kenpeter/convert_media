be0bf7b7932a53d6ce8623cd4211543f795cf31c
kenpeter <figo2476@hotmail.com>
2015-08-26 10:55:41 +1000

    update

--- convert_media.js
+++ convert_media.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
 // usage: 
-// 1. ./convert_media.js --mp3 --path /full_path_of_directory
-// 2. ./convert_media.js --mp4 --path /full_path_of_directory (to create mp4 for iDevice)
+// 1.  convert_media.js --mp3 --path /full_path_of_directory
+// 1.1  convert_media.js --mp3 --json input.json --path /media/kenpeter/3E3C68780C3F137A/input 
+// 2.  convert_media.js --mp4 --path /full_path_of_directory (to create mp4 for iDevice)
 
 // https://github.com/tj/commander.js
 var program = require('commander');
 var glob = require('glob');
 var spawn = require('child_process').spawn;
 var path = require('path');
+var fs = require("fs");
 
 var options;
 var my_path;
@@ -18,6 +20,7 @@ program
   .option('-a, --mp3', 'Convert mp4 or other media type files to mp3')
   .option('-b, --mp4', 'Convert other media type files to mp4')
   .option('-c, --path [path]', 'The path to media directory')
+  .option('-d, --json [json]', 'json contains start and duration of the mp3')
   .parse(process.argv);
 
 // Media direcotry path
@@ -30,7 +33,7 @@ else {
   console.log('Please provide the media direcotry path');
 }
 
-if(program.mp3) {
+if(program.mp3 && !program.json) {
   console.log('Processing mp3....');
 
   // options is optional
@@ -45,7 +48,7 @@ if(program.mp3) {
       var input_file_path = files[i];
       var file_json = path.parse(input_file_path);
       var file_directory = file_json.dir; 
-      var output_file_path = file_directory + '/' + file_json.name + '.mp3'; 
+      var output_file_path = file_directory + '/' + file_json.name + '_con' + '.mp3'; 
 
       console.log('Input file: ' + input_file_path);
       console.log('Output file: ' + output_file_path);      
@@ -60,6 +63,93 @@ if(program.mp3) {
     }
 
     process.exit(); 
+  })
+
+}
+else if(program.mp3 && program.json) {
+  console.log('Processing cut mp3....');
+
+  // Delete unwanted files, this needs to happen earlier.
+  var exec = require('child_process').exec;
+  exec('rm *_cut.mp3');
+  console.log('Delete *cut.mp3 files');
+
+  // options is optional
+  options = {
+  
+  };
+
+  my_path = program.path + '/**/*';
+
+  glob(my_path, options, function (er, files) {
+    var time_json = JSON.parse(fs.readFileSync(program.path + '/' + program.json, "utf8"));
+   
+    console.log('NOTE: number of json obj must match the number of converting songs!!!!'); 
+    console.log('Reading time json file: ' + program.json);
+    //console.log('Content of time json file:');
+    //console.log(time_json);
+    console.log('How many files to convert:');
+    console.log(files.length);
+
+    for(var i in files) {
+      var input_file_path = files[i];
+      var file_json = path.parse(input_file_path);
+      
+      if(file_json.ext == '.mp3') {
+        var file_directory = file_json.dir;
+        var output_file_path = file_directory + '/' + file_json.name + '_cut' + '.mp3';
+
+        // e.g. avconv -i RR119Accessibility.wav -ss 52:13:49 -t 01:13:52 RR119Accessibility-2.wav
+        var index = i - 2; // Because . and .., so it starts at 2?
+
+        console.log('Index: ' + index);
+        console.log('Input file: ' + input_file_path);
+        console.log('Output file: ' + output_file_path);
+
+        /*
+          Sample json file
+          [
+            {
+              "start": "00:00:10",
+              "length": "00:01:00"
+            },
+            {
+              "start": "00:00:10",
+              "length": "00:01:00"
+            },
+            {
+              "start": "00:00:10",
+              "length": "00:01:00"
+            },
+            {
+              "start": "00:00:10",
+              "length": "00:01:00"
+            }
+          ]
+
+          NOTE: the length is always the same, because only cut the start.
+          the number of object should match number of song
+        */
+        var time_obj = time_json[index];
+        var start_time = time_obj.start;
+        var duration = time_obj.length;
+
+        console.log('Start time and duration:');
+        console.log(time_obj.start + ' | ' + time_obj.length);
+
+        
+        // It runs parallel.
+        var child = spawn('avconv', [
+          '-i', input_file_path,
+          '-ss', start_time,
+          //'-t', duration, // We only cut the start
+          output_file_path
+        ]);
+        
+      }
+    }   
+
+    process.exit();
   })
 
 }
@@ -78,7 +168,7 @@ else if(program.mp4) {
       var input_file_path = files[i];
       var file_json = path.parse(input_file_path);
       var file_directory = file_json.dir;
-      var output_file_path = file_directory + '/' + file_json.name + '.mp4';
+      var output_file_path = file_directory + '/' + file_json.name + '_con' + '.mp4';
 
       console.log('Input file: ' + input_file_path);
       console.log('Output file: ' + output_file_path);
